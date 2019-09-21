94ca4715b87544b185bd2887aa1181b2ec3f94c6
BryceMiskin <46393816+BryceMiskin@users.noreply.github.com>
2019-09-20 18:45:42 -0600

    Update car_game.py

--- car_game.py
+++ car_game.py
@@ -1,21 +1,40 @@
-# Turn a car on and off
+######### TTHIS IS MADE FOR FUN. THE USER WILL BE ABLE TO INTEREACT WITH AN IMAGINARY CAR IN THIS PROGRAM #########
 import sys
 
 # To do list:
-# + Finish the "close" command
+# + Automatically close all doors when beginning to drive
 # + Status command reports if the car is on and driving
 # + Status command report which doors are open
 # + Automatically close all doors when beginning to drive
 # + 
 
-# This block of code is used in some of the following variables. Do not move it!
-print()
-name = input("Your name: ")
-car_name = input("Give me an epic car name: ")
-print()
-print(f"""Seriously {name}? I could do better than "{car_name}" and I'm a program...""")
+############################ VARIABLE STORAGE TO PREPARE INTRO ############################
 
-# The help menu and credits which can be recalled later.
+# For the "else" block in "if question_choice == "y" and what not"
+# Also used in the (y / n) below
+question_mispell = """\n(y / n) means to submit "y" for yes and "n" for no. Try again.\n"""
+
+############################ INTRO TO PREPARE THE VARIABLE STORAGE (INTERACTION INCLUDED) ############################
+
+name = input("\nYour name: ")
+
+while True:
+    car_name = input("Name your car: ")
+    car_name_verify = input(f"\nName your car {car_name}? \n(y / n): ").lower()
+    if car_name_verify == "y":
+        break
+    elif car_name_verify == "n":
+        print("\nAlright here's a second shot.")
+        continue
+    if car_name_verify != "y" "n":
+        print(question_mispell)
+        car_name_verify = input(f"Name your car {car_name}? \n(y / n): ")
+
+print(f"""\n{car_name} is an impressive car name, {name}!""")
+
+############################ VARIABLE STORAGE ############################
+
+# Users can call this variable later 
 help = (f"""
 Available Commands:
 On/Off - Turn {car_name} on or off
@@ -25,10 +44,6 @@ Status - Is {car_name} on?
 Help - Get this helpful list
 Quit - End the program""")
 
-creators = ["Bryce Miskin"]
-credits = f"Credit for this code goes to {creators}."
-
-# This variable is called later.
 door_list = ("""1 - Driver door
 2 - Passenger door
 3 - The hood
@@ -42,10 +57,12 @@ door_list = ("""1 - Driver door
 power = False
 driving = False
 
-# --------------------------------------------------
+creators = ["Bryce Miskin"]
+credits = f"Credit for this code goes to {creators}."
 
+############################ INTERACTION RESUMED ############################
 
-print('\nNever worry about CaPiTaLiZaTiOn in this program for now on. \nEnter "Help" for command options.\n')
+print('Never worry about CaPiTaLiZaTiOn in this program from now on. \nEnter "Help" for command options.\n')
 
 while True:
     command = input("> ").lower()
@@ -74,19 +91,14 @@ while True:
             if driving:
                 driving = False
                 print(f"{car_name} stopped.")
-
         if not power:
             print(f"{car_name} is off and not moving. Wierd flex to make me push the brake anyway but okay...")
     # Is the car on or off?
-    elif command == "status":
-        if power:
-            print(f"""{car_name} is ready to roll! \nTry "go" """)
-        elif not power:
-            print(f"{car_name} is off and awaiting your arrival.")
+
     # Open car doors, trunk, hood, or gasoline door. If a door is false then it is open.
     elif command == "open":
-        if power:
-            print(f"{car_name} cannot open anything while the car is on.")
+        if driving:
+            print(f"{car_name} cannot open anything while the car is driving.")
         if not power:
             print("\nOpen what? Enter a number.")
             print(door_list)
@@ -120,22 +132,70 @@ while True:
                 continue
             else:
                 print("That's not a number...")
+
     # Close car doors, trunk, hood, or gasoline door. If a door is true then it is closed and secure.
     elif command == "close":
         print("\nClose what? Enter a number.")
         print(door_list)
-        close_num = int(input ("Enter number: "))
-        if close_num = 1:
-            driver_door = True
-####### FINISH THE CLOSE COMMAND
+        close_num = int(input("Number: ").lower())
+        ## 1-driver door 2-Passenger 3- the hood 4- the trunk 5- gas door 6-all doors
+        ## True means closed and secured
+        if close_num == 1:
+            if not driver_door:
+                driver_door = True
+                print(f"")
+            if driver_door:
+                print(f"{name}'s {car_name}'s door already closed.")
+        elif close_num == 2:
+            if not passenger_door:
+                passenger_door = True
+                print(f"Okay, {car_name}'s passenger door closed.")
+            if passenger_door:
+                print(f"Passenger on {car_name} is already secured.")
+        elif close_num == 3:
+            if not hood:
+                hood = True
+                print(f"")
+            if hood:
+                print(f"")
+        elif close_num == 4:
+            if not trunk:
+                trunk = True
+                print(f"Hood closed. The children are trapped.")
+            if trunk:
+                print("The children already can't escape {car_name}. Hopefully they don't eat your groceries...")
+        elif close_num == 5:
+            if not gas_door:
+                gas_door = True
+                print("Gas door closed.")
+            if gas_door:
+                print(f"The gas door isn't isn't open... Nice try though, {name}.")
+        elif close_num == 6:
+                driver_door = True; passenger_door = True; hood = True; trunk = True; gas_door = True
+    elif command == "status":
+        if power:
+            print(f"""{car_name} is turned on! When {car_name} is on all doors close. \nTry "go" """)
+        if not power:
+            print(f"""{car_name} is off and awaiting your arrival.""")
+            print("See which doors are open and closed?")
+            question_choice = input("(y / n): ")
+            if question_choice == "y":
+                ###### FINISH
+                print()
+            if question_choice == "n":
+                break
+            else:
+                print()
+                ###### FINISH
+
+    # Close all doors
     elif command =="close all":
         driver_door = True; passenger_door = True; hood = True; trunk = True; gas_door = True
         print(f"All doors on {car_name} are locked and secured!")
     elif command == "help":
         print(help) # See the beginning of the code for the help variable.
     elif command == "quit":
-        print()
-        print(f"See you next time, {name}!")
+        print(f"\nSee you next time, {name}!")
         print(credits)
         sys.exit()
     # Unknown command?
